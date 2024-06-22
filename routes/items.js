4d1edcf5c883f2dbc06c0c3d5340dc9548583099
Sangwoo-Oh <sangwoooh1013@gmail.com>
2024-06-22 10:18:26 +0200

    implement getItemById

--- routes/items.js
+++ routes/items.js
@@ -6,5 +6,13 @@ router.get('', async (req, res) => {
     result = await Item.find({});
     res.json(result);
 })
+router.get('/:id', async (req, res) => {
+    try {
+        result = await Item.findById(req.params.id);
+        res.json(result);
+    } catch (err) {
+        res.status(404).send({ error: 'ERROR' })
+    }
+})
 
 module.exports = router;
