ea5e5475ed8e3adf129638d3d79821852a9a14a2
Sangwoo-Oh <sangwoooh1013@gmail.com>
2024-07-08 10:26:03 +0200

    modified seed.js

--- seed.js
+++ seed.js
@@ -1,16 +1,21 @@
 const config = require('./config/config');
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://'+ config.USERNAME +':' + config.PASSWORD + '@' + config.URI).then(()=>{console.log('successful connection!')}).catch((err)=>{console.log(err)});
-
 const Item = require("./model/item");
 const Category = require("./model/category");
 const Purchase = require("./model/purchase");
 
 class Seed {
     async seedData() {
-        await this.cleanDb();
-        this.generateItem();
+        return new Promise(async (resolve, reject) => {
+            try {
+                await mongoose.connect('mongodb+srv://'+ config.USERNAME +':' + config.PASSWORD + '@' + config.URI).then(()=>{console.log('successful connection!')}).catch((err)=>{console.log(err)});
+                await this.cleanDb();
+                await this.generateItem();
+                await resolve();
+            } catch(err) {
+                reject(err);
+            }
+        });
     }
 
     async cleanDb() {
@@ -30,7 +35,7 @@ class Seed {
             description: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects,',
             price: 10000,
             seller: new mongoose.Types.ObjectId('668170feaad528c8c2191628'),
-            image_url: 'https://marketplace-app-s3.s3.amazonaws.com/iphone13.jpg'
+            image_url: 'https://marketplace-app-s3.s3.amazonaws.com/seed/iphone13.jpg'
         }).save();
         new Item({
             public_data: new Date(2024, 7, 1),
@@ -39,7 +44,7 @@ class Seed {
             description: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects,',
             price: 30000,
             seller: new mongoose.Types.ObjectId('66851818b7c1219d3f54d439'),
-            image_url: 'https://marketplace-app-s3.s3.amazonaws.com/macbook.jpg'
+            image_url: 'https://marketplace-app-s3.s3.amazonaws.com/seed/macbook.jpg'
         }).save();
         new Item({
             public_data: new Date(2024, 7, 1),
@@ -48,10 +53,16 @@ class Seed {
             description: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects,',
             price: 8000,
             seller: new mongoose.Types.ObjectId('668170feaad528c8c2191628'),
-            image_url: 'https://marketplace-app-s3.s3.amazonaws.com/galaxy.jpg'
+            image_url: 'https://marketplace-app-s3.s3.amazonaws.com/seed/galaxy.jpg'
         }).save();
     }
 }
 
 const seed = new Seed();
-seed.seedData();
+seed.seedData().then(
+    async ()=>{
+        console.log('seed data created');
+    }
+).catch(
+    (err)=>{console.error(err);}
+);
